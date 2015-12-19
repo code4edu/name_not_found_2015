6265735a86a723eeb3638cf01143656656d4306c
ab245 <ab245@bk.ru>
2015-12-19 11:48:39 +0300

    Message

--- QuickTester/QuickTester.Windows/Questions/QuestionWithoutVariantsOfAnswer.cs
+++ QuickTester/QuickTester.Windows/Questions/QuestionWithoutVariantsOfAnswer.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace QuickTester.Questions
-{
-    public class QuestionWithoutVariantsOfAnswer : IQuestion
-    {
-        public string Question { get; set; }
-        private int rightAnswer;
+//namespace QuickTester.Questions
+//{
+//    public class QuestionWithoutVariantsOfAnswer : IQuestion
+//    {
+//        public string Question { get; set; }
+//        private int rightAnswer;
 
-        public QuestionWithoutVariantsOfAnswer(string question, int RightAnswer)
-        {
-            this.Question = question;
-            rightAnswer = RightAnswer;
-        }
-        //TODO: Ответ на вопрос вводится с клавиатуры. ОН обязательно числовой!!!
-        public bool TheRightAnswer(int i)
-        {
-            if (i == rightAnswer)
-                return true;
-            return false;
-        }
-    }
-}
+//        public QuestionWithoutVariantsOfAnswer(string question, int RightAnswer)
+//        {
+//            this.Question = question;
+//            rightAnswer = RightAnswer;
+//        }
+//        //TODO: Ответ на вопрос вводится с клавиатуры. ОН обязательно числовой!!!
+//        public bool TheRightAnswer(int i)
+//        {
+//            if (i == rightAnswer)
+//                return true;
+//            return false;
+//        }
+//    }
+//}
